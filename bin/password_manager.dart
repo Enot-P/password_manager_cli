5f027c7ffd4983f5d451b73d16fedabbf8a03616
Enot-P <nikita.ptashckin@yandex.ru>
2025-05-09 18:37:37 +0300

    Добавлено удаление пароля и его шифрование в файле

--- bin/password_manager.dart
+++ bin/password_manager.dart
@@ -1,11 +1,40 @@
+import 'dart:io';
+
 import 'package:args/args.dart';
 import 'package:password_manager/password_manager.dart';
 
-const String version = '0.0.1';
+// String version = Platform.version;
 
-void main(List<String> arguments) {
-  var pass = Password(service: "new");
+Future<void> main(List<String> arguments) async {
+  await Password.updateListPass();
+  if (arguments.isNotEmpty) {
+    switch (arguments.first) {
+      case '--help' || '-h':
+        print('TO-DO add help');
+      case '--version' || '-v':
+        print(Platform.version);
+      case '--contacts' || '-c':
+        print("Contacts\n");
+        print('Email: nikita.ptashkin2@gmail.com');
+      case '-getAllPass':
+        Password.getAllPasswords();
+      case '-createPass':
+        if (arguments[1].isNotEmpty) {
+          Password newPass = Password(service: arguments[1]);
+          print(
+            'Password successful added. Your new password "${newPass.name}":',
+          );
+          print(newPass.password);
+        }
+      case '-delete' || '-d':
+        if (arguments[1].isNotEmpty) {
+          Password newPass = Password(service: arguments[1]);
+          newPass.deletePassword();
+        }
 
-  //pass.generatePassword();
-  print(Password.getPasswords());
+      case _:
+        print(Password.getPassword(arguments.first));
+    }
+  }
+  exit(0);
 }
--- bin/test.dart
+++ bin/test.dart
@@ -1,37 +1,23 @@
-import 'dart:convert';
-import 'dart:io';
 import 'dart:math';
+
 import 'package:encrypt/encrypt.dart';
-import 'package:encrypt/encrypt_io.dart';
-import 'package:pointycastle/asymmetric/api.dart';
 
-Future<void> main() async {
-  // File file = File("public.pem");
-  // File file_2 = File("private.pem");
-  // await file.create(recursive: true);
-  // await file_2.create(recursive: true);
+void main(List<String> args) {
+  final plainText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+  final key = Key.fromUtf8(_generateKey(32));
+  print(_generateKey(32));
+  final iv = IV.fromLength(16);
 
-  // final publicKey = await parseKeyFromFile<RSAPublicKey>(file.path);
-  // final privKey = await parseKeyFromFile<RSAPrivateKey>(file_2.path);
+  final encrypter = Encrypter(AES(key));
 
-  // final plainText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
-  // final key = Key.fromUtf8('my 32 length key................');
-  // final iv = IV.fromLength(16);
+  final encrypted = encrypter.encrypt(plainText, iv: iv);
+  final decrypted = encrypter.decrypt(encrypted, iv: iv);
 
-  // final encrypter = Encrypter(AES(key));
-
-  // final encrypted = encrypter.encrypt(plainText, iv: iv);
-  // final decrypted = encrypter.decrypt(encrypted, iv: iv);
-
-  // print(decrypted); // Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  // print(
-  //   encrypted.base64,
-  // ); // R4PxiU3h8YoIRqVowBXm36ZcCeNeZ4s1OvVBTfFlZRdmohQqOO9EbgbrSwiq0EYz0aBdljHSC/rci2854Qa+nugbhKjidlezNplsEqOxR+pr1RtICZGAtv0YGevJBaRaHS17eHuj7GXo1CM3PR6pjGxrorcwR5Q7/bVEePESsimMbhHWF+AkDIX4v0CwKx9lgaTBgC8/yJKiLmQkyDCj64J3JSE=
-  print(generateKey(100));
+  print(decrypted); // Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  print(encrypted.base64);
 }
 
-String generateKey(int length) {
-  //33 - 122
+String _generateKey(int length) {
   String result = "";
   const List<String> unusedPasswordSymbols = [
     '!',
